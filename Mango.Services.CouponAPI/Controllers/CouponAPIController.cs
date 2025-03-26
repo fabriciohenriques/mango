5b564e806f50dcf4f09fd06d0ec25fd491300097
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-03-26 14:03:04 -0300

    24

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -3,6 +3,7 @@ using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
@@ -25,11 +26,11 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpGet]
-        public ResponseDto Get()
+        public async Task<ResponseDto> Get()
         {
             try
             {
-                IEnumerable<Coupon> obj = _db.Coupons.ToList();
+                var obj = await _db.Coupons.ToListAsync();
                 _response.Result = _mapper.Map<IEnumerable<CouponDto>>(obj);
             }
             catch (Exception ex)
@@ -42,12 +43,87 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public ResponseDto Get(int id)
+        public async Task<ResponseDto> Get(int id)
         {
             try
             {
-                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+                var obj = await _db.Coupons.FirstAsync(c => c.CouponId == id);
                 _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet("GetByCode/{code}")]
+        public async Task<ResponseDto> Get(string code)
+        {
+            try
+            {
+                var obj = await _db.Coupons.FirstAsync(c => c.CouponCode.ToLower() == code.ToLower());
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<ResponseDto> Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var obj = _mapper.Map<Coupon>(couponDto);
+                await _db.Coupons.AddAsync(obj);
+                await _db.SaveChangesAsync();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<ResponseDto> Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                await _db.SaveChangesAsync();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ResponseDto> Delete(int id)
+        {
+            try
+            {
+                var obj = _db.Coupons.First(c => c.CouponId == id);
+                _db.Coupons.Remove(obj);
+                await _db.SaveChangesAsync();
             }
             catch (Exception ex)
             {
