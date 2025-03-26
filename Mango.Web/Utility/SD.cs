5c272e629f03c25480c2a83c7973f95cc4e0a702
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-03-26 19:33:12 -0300

    26

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            DELETE,
+            GET,
+            POST,
+            PUT,
+        }
+    }
+}
