4f89a064655f66031a7dd890d1365c42821f1282
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-03-27 14:28:59 -0300

    36

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -22,5 +22,10 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
     }
 }
