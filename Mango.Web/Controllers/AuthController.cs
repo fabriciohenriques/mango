ee61258b8e40855fc24bbdb10789b9c44f889207
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-03-31 13:34:10 -0300

    56

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
