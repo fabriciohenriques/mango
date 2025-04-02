0169c8e13198f40dbfc1a6ca563b804b19301b1e
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-04-02 11:05:09 -0300

    84

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CartAPIController(
+            AppDbContext db,
+            IMapper mapper)
+        {
+            _response = new ResponseDto();
+            _db = db;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+            return new ResponseDto();
+        }
+    }
+}
