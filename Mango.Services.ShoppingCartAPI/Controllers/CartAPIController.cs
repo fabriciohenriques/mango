cf9faca19faec1f05afdb76af6fb31a1b67de2f5
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-04-02 12:09:54 -0300

    85

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -26,7 +28,41 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         [HttpPost]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
-            return new ResponseDto();
+            try
+            {
+                var cartHeaderFromDb = await _db.CartHeaders.FirstOrDefaultAsync(c => c.UserId == cartDto.CartHeader.UserId);
+
+                if (cartHeaderFromDb == null)
+                {
+                    //create header and detail
+                    var cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+                    var cartHeaderToDb = await _db.AddAsync(cartHeader);
+                    var cartDetail = _mapper.Map<CartDetail>(cartDto.CartDetails.First());
+                    cartDetail.CartHeader = cartHeader;
+                    var cartDetailToDb = await _db.AddAsync(cartDetail);
+
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    var cartDetailsFromDb = await _db.CartDetails.FirstAsync(cd => cd.ProductId == cartDto.CartDetails.First().ProductId && cd.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+                    if (cartDetailsFromDb == null)
+                    {
+                        //create cartdetail
+                    }
+                    else
+                    {
+                        //update count
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+
+            return _response;
         }
     }
 }
