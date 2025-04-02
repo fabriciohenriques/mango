a5e221a533eb20ab44779e954a9d24ded84355d4
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-04-02 10:46:22 -0300

    82

---
+++ Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public Guid UserId { get; set; }
+        [StringLength(10)]
+        public string? CouponCode { get; set; }
+
+        [NotMapped]
+        public double Discount { get; set; }
+        [NotMapped]
+        public double CartTotal { get; set; }
+    }
+}
