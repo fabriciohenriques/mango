dd9214786cfbe8afb57cb971553b5af449c4cd7b
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-04-03 10:50:14 -0300

    87

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -2,10 +2,8 @@
 using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.PortableExecutable;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -64,6 +62,41 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 }
 
                 _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+
+            return _response;
+        }
+
+        [HttpPost("RemoveCartDetail")]
+        public async Task<IActionResult> RemoveCartDetail([FromBody] int carDetailId)
+        {
+            try
+            {
+                var cartDetail = await _db.CartDetails.FirstOrDefaultAsync(cd => cd.CartDetailsId == carDetailId);
+
+                if (cartDetail != null)
+                {
+                    _db.Remove(cartDetail);
+
+                    var totalDetailsOfCart = await _db.CartDetails.CountAsync(cd => cd.CartHeaderId == cartDetail.CartHeaderId);
+
+                    if (totalDetailsOfCart == 1)
+                    {
+                        var cartHeader = await _db.CartHeaders.FirstAsync(ch => ch.CartHeaderId == cartDetail.CartHeaderId);
+                        _db.Remove(cartHeader);
+                    }
+                    await _db.SaveChangesAsync();
+
+                    _response.IsSuccess = true;
+                }
+
+                _response.Message = "Cart Detail not found.";
+                _response.IsSuccess = false;
             }
             catch (Exception ex)
             {
