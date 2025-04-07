da7ee37feb79dd1267b8a1c24e623e4959565b29
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-04-07 15:57:23 -0300

    110

---
+++ Mango.Services.EmailAPI/Models/EmailLogger.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.EmailAPI.Models
+{
+    public class EmailLogger
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(256)]
+        public required string Email { get; set; }
+        [Required]
+        public required string Message { get; set; }
+        public DateTime? EmailSent { get; set; }
+    }
+}
