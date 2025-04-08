d53493fba956e116cd2f21d38faff43bbc40221a
Fabrício Henriques <fabriciosiqueirahenriques@gmail.com>
2025-04-08 16:35:42 -0300

    122

---
+++ Mango.Services.OrderAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.OrderAPI.Models.Dto
+{
+    public class CartDto
+    {
+        public required CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailDto>? CartDetails { get; set; }
+    }
+}
